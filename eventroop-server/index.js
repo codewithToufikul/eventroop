8ca38e6ff6843147face7c7aea85bee0620f615c
Toufikul Islam <imranmeg2356@gmail.com>
2025-06-30 14:39:43 +0600

    chore: update .gitignore to exclude node_modules and env files

---
+++ eventroop-server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const databaseUser = process.env.DATABASE_USER;
+const databasePassword = process.env.DATABASE_PASSWORD;
+const uri =
+  `mongodb+srv://${databaseUser}:${databasePassword}@cluster0.ivo4yuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
