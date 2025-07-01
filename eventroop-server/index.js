381fecf4380c28476e4f54baee079c494af25f1e
Toufikul Islam <imranmeg2356@gmail.com>
2025-07-01 12:35:49 +0600

    almost everything done

--- eventroop-server/index.js
+++ eventroop-server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://eventroop-client.vercel.app",
   credentials: true
 }));
 
